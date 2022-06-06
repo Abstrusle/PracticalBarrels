16079905386965aaebe450899adc4219f38ee672
Abstrusle <40145743+Abstrusle@users.noreply.github.com>
2022-06-06 05:55:09 -0600

    Added comments.

--- src/main/java/com/puradox/practicalbarrels/PracticalBarrelsClient.java
+++ src/main/java/com/puradox/practicalbarrels/PracticalBarrelsClient.java
@@ -16,26 +16,25 @@ import net.minecraft.text.Text;
 public class PracticalBarrelsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> { //Occurs after any Screen is initialized. Barrel inventory isn't prepared yet.
             if (screen.getTitle().getString().toLowerCase().contains("barrel") &&
-                    screen instanceof GenericContainerScreen) {
+                    screen instanceof GenericContainerScreen) { //Is it a chest-like inventory containing the name 'Barrel'?
                 ClientPlayerEntity player = client.player;
                 assert player != null;
                 ItemStack inputItem = player.getMainHandStack();
-                ScreenEvents.afterTick(screen).register((AfterTick) -> {
+                ScreenEvents.afterTick(screen).register((AfterTick) -> { //After inventory is available.
                     GenericContainerScreen invScreen = (GenericContainerScreen) screen;
                     ClientPlayerInteractionManager interactionManager = MinecraftClient.getInstance().interactionManager;
                     Screens.getButtons(screen).add(new ButtonWidget(screen.width / 2 - 100, (screen.height / 2), 10, 10, Text.of("^"),
                             (PressAction) -> {
                                 for (int i = 0; i <= invScreen.getScreenHandler().getInventory().size() - 9; i++) {
-
                                     doItemsCheck(invScreen.getScreenHandler().getInventory().getStack(i), player, invScreen, interactionManager);
                                 }
                             }));
-                    if (!player.getMainHandStack().isEmpty()) {
-                        if (invScreen.getScreenHandler().getInventory().isEmpty() && !inputItem.isEmpty()) {
+                    if (!player.getMainHandStack().isEmpty()) { //Empty hands will always open barrels normally.
+                        if (invScreen.getScreenHandler().getInventory().isEmpty() && !inputItem.isEmpty()) { //Right-clicking an empty barrel.
                             doItemsCheck(inputItem, player, invScreen, interactionManager);
-                        } else if (!inputItem.isEmpty() && invScreen.getScreenHandler().getInventory().count(inputItem.getItem()) > 0) {
+                        } else if (!inputItem.isEmpty() && invScreen.getScreenHandler().getInventory().count(inputItem.getItem()) > 0) { //Right-clicking with a related item.
                             doItemsCheck(inputItem, player, invScreen, interactionManager);
                             screen.close();
                         }
@@ -55,7 +54,7 @@ public class PracticalBarrelsClient implements ClientModInitializer {
                         SlotActionType.QUICK_MOVE, player);
             }
         }
-        for(int i=41; i<45; i++) {
+        for(int i=41; i<45; i++) { //For some reason, the above loop doesn't acknowledge slots 41-44.
             if(player.playerScreenHandler.getSlot(i).getStack().getItem().equals(inputItem.getItem())) {
                 assert interactionManager != null;
                 interactionManager.clickSlot(
